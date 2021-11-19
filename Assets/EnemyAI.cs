c14af7e20d184a0debddca8c9798c81baebd419f
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-11-19 13:54:23 -0500

    Muzzle flash added to weapons, enemy shooting coroutine created

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -6,23 +6,40 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     public Transform chaseThis;
-    NavMeshAgent agent;
     public Transform[] gunList;
+    public GameObject muzzleEffect;
+    NavMeshAgent agent;
+    int fireNext = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         StartCoroutine(AIThink());
+        StartCoroutine(RateOfFire());
     }
 
-    // Update is called once per frame
     IEnumerator AIThink()
     {
         while (true)
         {
             agent.destination = chaseThis.position;
             yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    IEnumerator RateOfFire()
+    {
+        while (true)
+        {
+            Debug.Log("Fire " + fireNext);
+            GameObject.Instantiate(muzzleEffect, gunList[fireNext].position, gunList[fireNext].rotation);
+            fireNext++;
+            if(fireNext >= gunList.Length)
+            {
+                fireNext = 0;
+            }
+            yield return new WaitForSeconds(0.5f);
         }
     }
 }
