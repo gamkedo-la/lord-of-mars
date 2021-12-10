d9dff2e18baefb6988b146485808ef9f828cb3ce
SamuelAxon <samuelaxon@gmail.com>
2021-12-09 23:14:39 -0600

    Start/main menu fully implemented

---
+++ Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    public Color32 startingColor;
+    public Color32 highlightColor;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("MainMenuButton(): OnPointerEnter() was called for object " + name);
+        gameObject.GetComponent<Image>().color = highlightColor;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("MainMenuButton(): OnPointerExit() was called for object " + name);
+        gameObject.GetComponent<Image>().color = startingColor;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (name == "StartButton")
+        {
+            SceneManager.LoadScene("Tutorial");
+        }
+        else if (name == "QuitButton")
+        {
+            Application.Quit();
+        }
+    }
+}
