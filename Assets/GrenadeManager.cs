c46bc1764ba7abd19f02f0063f71de1327f55613
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-12-20 23:19:08 -0500

    Grenades now fully operational, grenades spawn when enemies are present and player hasn't moved within a random.range amount of seconds

---
+++ Assets/GrenadeManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeManager : MonoBehaviour
+{
+    public GameObject grenadePrefab;
+    Vector3 lastPosition = Vector3.zero;
+
+    void Start()
+    {
+        StartCoroutine(GrenadeMeTest());
+    }
+
+
+    IEnumerator GrenadeMeTest()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(Random.Range(3.0f, 7.0f)); //how often do we check throwing a grenade 
+            Vector3 posNow = transform.position;
+            float distMoved = Vector3.Distance(posNow, lastPosition);
+            lastPosition = posNow;
+            Debug.Log("considering grenade drop");
+            if (distMoved < 2.0f) //how far the player has moved between checks to not get grenaded 
+            {
+
+            
+                Debug.Log("player didn't move enough");
+                Collider[] throwRadius = Physics.OverlapSphere(transform.position, 30.0f); //how far AI can throw a grenade
+                bool foundThrower = false;
+                for (int i = 0; i < throwRadius.Length; i++)
+                {
+                    EnemyAI enemyScript = throwRadius[i].GetComponent<EnemyAI>(); //might need to check for children of parent?
+                    if (enemyScript)
+                    {
+                        foundThrower = true;
+                        Debug.Log("found someone to throw grenade");
+                        break;
+                    }
+                }
+                if(foundThrower)
+                {
+                    //right now spawns in front of camera- needs UI indicator- should happen off camera at that point
+                   GameObject grenGO = GameObject.Instantiate(grenadePrefab, Camera.main.transform.position + Camera.main.transform.forward * 2.0f, Quaternion.identity);
+                    Rigidbody rb = grenGO.GetComponent<Rigidbody>();
+                    rb.angularVelocity = Random.insideUnitSphere;
+                    rb.AddForce(Random.onUnitSphere * 100.0f);
+                }
+            }
+        }
+    }
+
+}
--- Assets/Scripts/TimedExplode.cs
+++ Assets/Scripts/TimedExplode.cs
@@ -9,21 +9,27 @@ public class TimedExplode : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(ExplodeAfterDelay());
     }
 
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator ExplodeAfterDelay()
     {
-        if(Input.GetKeyDown(KeyCode.B))
+        yield return new WaitForSeconds(3.0f);
+        Debug.Log("grenade boom");
+        GameObject.Instantiate(blastEffect, transform.position, Quaternion.identity);
+        Collider[] blastedRadius = Physics.OverlapSphere(transform.position, 10.0f);
+        for(int i = 0; i < blastedRadius.Length; i++)
         {
-            Debug.Log("grenade boom");
-            GameObject.Instantiate(blastEffect, transform.position, Quaternion.identity);
-            Collider[] blastedRadius = Physics.OverlapSphere(transform.position, 10.0f);
-            for(int i = 0; i < blastedRadius.Length; i++)
+            Damageable damageScript = blastedRadius[i].GetComponent<Damageable>(); //might need to check for children of parent?
+            if(damageScript) //todo confirm line of sight with raycast 
             {
                 Debug.Log(blastedRadius[i].gameObject.name);
+                Vector3 hurtVect = (blastedRadius[i].transform.position - transform.position).normalized;
+                damageScript.TakeDamage(30.0f, hurtVect);
             }
-        }
-    }
+        } //end of for 
+        Destroy(gameObject);
+    } //end of function 
+
 }
