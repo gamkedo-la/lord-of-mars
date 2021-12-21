25cb6834b74ac7e65c194c61a5a4fe2d3cbe91e3
Jeremiah Franczyk <thefranczyk@gmail.com>
2021-12-20 22:42:33 -0500

    grenade work in progress, particle effect by Chris

--- Assets/Scripts/TimedExplode.cs
+++ Assets/Scripts/TimedExplode.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TimedExplode : MonoBehaviour
 {
+    public GameObject blastEffect;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,15 @@ public class TimedExplode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            Debug.Log("grenade boom");
+            GameObject.Instantiate(blastEffect, transform.position, Quaternion.identity);
+            Collider[] blastedRadius = Physics.OverlapSphere(transform.position, 10.0f);
+            for(int i = 0; i < blastedRadius.Length; i++)
+            {
+                Debug.Log(blastedRadius[i].gameObject.name);
+            }
+        }
     }
 }
