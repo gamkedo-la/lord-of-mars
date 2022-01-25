a1e5bef1c65596a437698ebba1cde52d9f41d08c
Jeremiah Franczyk <thefranczyk@gmail.com>
2022-01-24 23:35:59 -0500

    Added singleton to audiomanager to persist across scenes

--- Assets/Scripts/Sound/AudioManager.cs
+++ Assets/Scripts/Sound/AudioManager.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-
     public Sound[] sounds;
+
+    public static AudioManager instance;
 
     private void Awake()
     {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
