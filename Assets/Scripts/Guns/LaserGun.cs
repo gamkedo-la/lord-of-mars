4528d3b2fc78b58910e02a370c161f28672695ea
Armando Navarrete <viking805fan@yahoo.com>
2022-01-26 13:32:38 -0800

    W.I.P. Laser Gun Beam

--- Assets/Scripts/Guns/LaserGun.cs
+++ Assets/Scripts/Guns/LaserGun.cs
@@ -6,35 +6,37 @@ public class LaserGun : MonoBehaviour
 {
     Transform cameraTransform;
     LayerMask bulletMask;
-    public GameObject laserbeamPrefab;
     public Transform fireFrom;
-
+    public GameObject laserBeam;
+    private LineRenderer line;
 
     // Start is called before the first frame update
     void Start()
     {
         cameraTransform = Camera.main.transform;
         bulletMask = LayerMask.GetMask("Default", "TransparentFX", "Water", "Grapple", "Ground");
+        laserBeam.SetActive(false);
+        line = laserBeam.GetComponent<LineRenderer>();
     }
-
-
 
     public void Shoot()
     {
-        RaycastHit rhInfo;
-        FindObjectOfType<AudioManager>().SoundPlay("MainGun");
+        Debug.Log("laserbeam fire");
+        laserBeam.SetActive(true);
+        line.SetPosition(0, fireFrom.position);
+        line.SetPosition(1, fireFrom.position+fireFrom.forward*10.0f);
+
+        /*RaycastHit rhInfo;
         if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out rhInfo, 200.0f, bulletMask))
         {
             //Debug.Log(rhInfo.collider.name);
             Damageable hurtScript = rhInfo.collider.GetComponentInParent<Damageable>();
-            FindObjectOfType<AudioManager>().SoundPlay("BulletHit");
             if (hurtScript)
             {
                 hurtScript.TakeDamage(25.0f, cameraTransform.forward);
             }
-            Instantiate(laserbeamPrefab, fireFrom.transform.position, Quaternion.identity);
-        }
-        //GameObject tempGO = GameObject.Instantiate(laserbeamPrefab, fireFrom.transform.position, fireFrom.transform.rotation);
-    }
+            Instantiate(laserbeamPrefab, rhInfo.point + rhInfo.normal * 0.1f, Quaternion.identity);
+        }*/
 
+    }
 }
