01f7646a198e6c1d0011cd5e35a641d123500fb2
Armando Navarrete <viking805fan@yahoo.com>
2022-02-28 20:06:51 -0800

    Working AI For Artillery Mech

---
+++ Assets/Scripts/Enemies/ArtilleryBullet.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtilleryBullet : MonoBehaviour
+{
+
+    public float speed;
+
+    private Transform player;
+    private Vector3 target;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        target = new Vector3(player.position.x, player.position.y, player.position.z);
+
+
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if(transform.position.x == target.x && transform.position.y == target.y && transform.position.z == target.z)
+        {
+            DestroyArtilleryBullet();
+
+        }
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            DestroyArtilleryBullet();
+        }
+
+    }
+
+
+    void DestroyArtilleryBullet()
+    {
+        Destroy(gameObject);
+    }
+
+}
